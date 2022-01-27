b669e4967e75cce620fcfcc76d01e25afaf70bd4
Oktovivian <68942070+Oktovivian@users.noreply.github.com>
2022-01-27 21:27:21 +0700

    Add files via upload

---
+++ variable.dart
@@ -0,0 +1,15 @@
+void main() {
+  // membuat variabel dengan tipe data
+  String nama = "Vian";
+  int umur = 18;
+  double tinggi = 173.67;
+  bool sudahMenikah = false;
+
+  // membuat variabel dengan kata kunci var
+  var alamat = "Lamongan, Indonesia";
+
+  // mencetak variabel
+  print("Nama saya $nama. Umur $umur. Tinggi $tinggi.");
+  print("Menikah: $sudahMenikah");
+  print("Alamat: $alamat");
+}
