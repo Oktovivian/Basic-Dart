8d69dca646ff5a3c7d15881096c95899983484fe
Oktovivian <68942070+Oktovivian@users.noreply.github.com>
2022-01-28 07:55:05 +0700

    Add files via upload

---
+++ perulangan_foreach.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+main() {
+  var languages = ["C", "C++", "Java", "Dart", "Python"];
+
+  for (var language in languages) {
+    print(language);
+  }
+
+  print("Total bahasa: ${languages.length}");
+}
